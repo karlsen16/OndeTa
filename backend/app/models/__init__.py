d307652ec37493f9440af5314f0fcfc181d83a52
lrdt <l.rdt@pm.me>
2026-04-13 17:43:51 -0300

    feat: criação da versão inicial dos models, arquivos básicos de configuração do app para se conectar com o banco de dados, configuração do alembic e da primeira versão das migrations

---
+++ backend/app/models/__init__.py
@@ -0,0 +1,3 @@
+from .user import User
+from .pet import Pet
+from .image import Image
---
+++ backend/app/models/image.py
@@ -0,0 +1,9 @@
+from app.extensions import db
+
+class Image(db.Model):
+    __tablename__ = "images"
+
+    id = db.Column(db.Integer, primary_key=True)
+    url = db.Column(db.String(255), nullable=False)
+
+    pet_id = db.Column(db.Integer, db.ForeignKey("pets.id"), nullable=False)
