676d361d18cd91771668b80c02c0cd1f167a4524
Arih_ <arianecrmnc@gmail.com>
2026-04-18 22:37:48 -0300

    feat: adicionando página de login + CORS

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -1,10 +1,13 @@
 from flask import Flask
+from flask_cors import CORS
 from .config import Config
 from .extensions import db
 
 def create_app():
     app = Flask(__name__)
     app.config.from_object(Config)
+
+    CORS(app, origins=['http://localhost:5173'])
 
     db.init_app(app)
     from app.models import User, Pet, Image
